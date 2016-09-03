cd3003cb4ec4077c26ae22cebecc990dfe2384bf
carndog <jason.carney1@gmail.com>
2016-09-03 22:26:08 +0100

    First commit

---
+++ Cardbox/CardboxTests/CardboxTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardboxTests
+{
+    [TestClass]
+    public class CardboxTests
+    {
+        [TestMethod]
+        public void EvaluateText()
+        {
+        }
+    }
+}
