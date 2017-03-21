338a1222f60bcec12258b39c9ebb3655ee01b199
carndog <jason.carney1@gmail.com>
2017-03-21 20:10:15 +0000

    Delete old folder

--- Cardbox/CardboxDefinition/CardboxDefinitionContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Data.Entity;
-
-namespace CardboxDefinition
-{
-    internal class CardboxDefinitionContext : DbContext
-    {
-        public DbSet<CardboxDefinition> Definitions { get; set; }
-    }
-}
