4db9c261fab8b96cbf15684dad921f017932cc6a
jason <jason.carney1@gmail.com>
2023-10-12 20:45:36 +0100

    quick feature add for refactor next

--- BonusAccumulator/BonusAccumulator/Program.cs
+++ BonusAccumulator/BonusAccumulator/Program.cs
@@ -1,14 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using BonusAccumulator;
 using BonusAccumulator.WordServices;
 using BonusAccumulator.WordServices.Factories;
-using BonusAccumulator.WordServices.Helpers;
 
 WordService wordService = WordServiceFactory.Create();
+QuizService quizService = new();
 
 string? command = "";
 HashSet<string> sessionWords = new HashSet<string>();
 HashSet<string> addedWords = new HashSet<string>();
+List<string> lastResult = new List<string>();
 Random random = new Random((int)DateTime.Now.Ticks);
 string? reply = null;
 
@@ -22,11 +24,15 @@ const string QuizSessionCommand = "qs";
 const string QuizWordsCommand = "qw";
 const string EndQuizSessionCommand = "eqs";
 const string AddWordCommand = "add";
+const string AddLastWordsCommand = "addlast";
+const string StoreAndClearAdded = "sca";
 const string HelpCommand = "help";
 const string CommandsText = $"Commands: Anagram: {AnagramCommand}, Build: {BuildCommand}, Pattern: {PatternCommand}, " +
-                            $"Distance: {DistanceCommand}, Alphagram Distance: {AlphagramDistanceCommand}," +
+                            $"Distance: {DistanceCommand}, Alphagram Distance: {AlphagramDistanceCommand}, " +
                             $"Quiz Session: {QuizSessionCommand}, Quiz Words: {QuizWordsCommand}, End Quiz: {EndQuizSessionCommand}, " +
-                            $"Add Word: {AddWordCommand}, End Exit: {ExitCommand}:   ";
+                            $"Add Words: {AddWordCommand}, Add Last Words: {AddLastWordsCommand} " +
+                            $"Store and clear added: {StoreAndClearAdded}, " +
+                            $"End Exit: {ExitCommand}:";
 
 Console.Write(CommandsText);
 
@@ -48,6 +54,8 @@ while (command == null || !command.Equals(ExitCommand, StringComparison.CurrentC
             command = Console.ReadLine();
             Answer anagram = wordService.Anagram(command);
             sessionWords.UnionWith(anagram.Words);
+            lastResult.Clear();
+            lastResult.AddRange(anagram.Words);
             Console.WriteLine(string.Join(",", anagram.Words));
             break;
         case BuildCommand:
@@ -59,73 +67,52 @@ while (command == null || !command.Equals(ExitCommand, StringComparison.CurrentC
             command = Console.ReadLine();
             Answer pattern = wordService.Pattern(command);
             sessionWords.UnionWith(pattern.Words);
+            lastResult.Clear();
+            lastResult.AddRange(pattern.Words);
             Console.WriteLine(string.Join(",", pattern.Words));
             break;
         case DistanceCommand:
             command = Console.ReadLine();
             Answer distance = wordService.Distance(command);
             sessionWords.UnionWith(distance.Words);
+            lastResult.Clear();
+            lastResult.AddRange(distance.Words);
             Console.WriteLine(string.Join(",", distance.Words));
             break;
         case AlphagramDistanceCommand:
             command = Console.ReadLine();
             Answer alphagramDistance = wordService.AlphagramDistance(command);
             sessionWords.UnionWith(alphagramDistance.Words);
+            lastResult.Clear();
+            lastResult.AddRange(alphagramDistance.Words);
             Console.WriteLine(string.Join(",", alphagramDistance.Words));
             break;
         case QuizSessionCommand:
-            if (sessionWords.Count != 0)
-            {
-                while (reply != EndQuizSessionCommand)
-                {
-                    int next = random.Next(1, sessionWords.Count);
-                    string answer = sessionWords.GetNthElement(next);
-                    Answer sessionQuiz = wordService.Anagram(answer);
-                    Console.WriteLine($"{answer.ToAlphagram()} {sessionQuiz.Words.Count}");
-                    reply = Console.ReadLine();
-                    string[] answers = reply.ToUpper().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                    List<string> list = answers.Union(sessionQuiz.Words).ToList();
-                    Console.WriteLine();
-                    Console.WriteLine(list.Count == sessionQuiz.Words.Count ? "Correct" : "Wrong");
-                    Console.WriteLine(string.Join(",", sessionQuiz.Words));
-                    Console.WriteLine();
-                }
-                Console.WriteLine("Quiz over");
-            }
+            quizService.RunQuiz(sessionWords, EndQuizSessionCommand, random, wordService, Console.WriteLine, Console.ReadLine);
             break;
         case QuizWordsCommand:
-            if (addedWords.Count != 0)
-            {
-                reply = null;
-                while (reply != EndQuizSessionCommand)
-                {
-                    int next = random.Next(1, addedWords.Count);
-                    string answer = addedWords.GetNthElement(next);
-                    Answer sessionQuiz = wordService.Anagram(answer);
-                    Console.WriteLine($"{answer.ToAlphagram()} {sessionQuiz.Words.Count}");
-                    reply = Console.ReadLine();
-                    string[] answers = reply.ToUpper().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-                    List<string> list = answers.Union(sessionQuiz.Words).ToList();
-                    Console.WriteLine();
-                    Console.WriteLine(list.Count == sessionQuiz.Words.Count ? "Correct" : "Wrong");
-                    Console.WriteLine(string.Join(",", sessionQuiz.Words));
-                    Console.WriteLine();
-                }
-                Console.WriteLine("Quiz over");
-            }
+            quizService.RunQuiz(addedWords, EndQuizSessionCommand, random, wordService, Console.WriteLine, Console.ReadLine);
+            break;
+        case StoreAndClearAdded:
+            string name = DateTime.Now.ToString("s").Replace(":", "");
+            using (StreamWriter writer = new($@"C:\\WordFromBonusAccumulator\session{name}.txt"))
+                writer.WriteLine(string.Join(Environment.NewLine, addedWords));
+            addedWords.Clear();
+            Console.WriteLine($"saved at {name}");
             break;
         case AddWordCommand:
             string readLine = Console.ReadLine() ?? string.Empty;
-            Answer addedWord = wordService.Pattern(readLine.ToUpper());
-            switch (addedWord.Words.Count)
-            {
-                case 0:
-                    Console.WriteLine("Added word invalid");
-                    break;
-                default:
-                    addedWords.UnionWith(new[] { readLine.ToUpper() });
-                    break;
-            }
+            string[] words = readLine.ToUpper().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            List<string> filtered = words.Where(x => wordService.Pattern(x).Words.Count > 0).ToList();
+            if (filtered.Count == 0)
+                Console.WriteLine("No valid words to add");
+            else
+                addedWords.UnionWith(filtered);
+
+            break;
+        case AddLastWordsCommand:
+            addedWords.UnionWith(lastResult);
+            lastResult.Clear();
             break;
     }
 }
