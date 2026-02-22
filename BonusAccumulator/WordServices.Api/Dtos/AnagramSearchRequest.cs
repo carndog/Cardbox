29ceec03da7f5e60074325841e917579a468e9bb
jason <jason.carney1@gmail.com>
2026-02-22 00:25:51 +0000

    Support wildcards on basic search page

--- BonusAccumulator/WordServices.Api/Dtos/AnagramSearchRequest.cs
+++ BonusAccumulator/WordServices.Api/Dtos/AnagramSearchRequest.cs
@@ -7,7 +7,7 @@ public record AnagramSearchRequest
     [Required]
     [MinLength(2)]
     [MaxLength(15)]
-    [RegularExpression(@"^[A-Za-z?]+$")]
+    [RegularExpression(@"^[A-Za-z?.]+$")]
     public string Rack { get; init; } = string.Empty;
 
     public SearchMode Mode { get; init; } = SearchMode.Anagram;
